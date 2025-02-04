ef163c09b5f2b94fb9d6a61ad92b9a0ac3648f89
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-04 02:04:35 +0000

    done

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='subpr',
     version='0.0.1',
-    description='subprocess.run & os.chdir python shell libs (shell/exec/load/compile)",
+    description='subprocess.run & os.chdir python shell libs (shell/exec/load/compile)',
     author='du7ec',
     author_email='dutec6834@gmail.com',
     url='https://github.com/FarAway6834/SubprunShell',
