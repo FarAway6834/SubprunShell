c61e40f587dc8792b1686a6990ac11ce0376503e
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-04 10:23:07 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='subpr',
+    version='0.0.1',
+    description='subprocess.run & os.chdir python shell libs (shell/exec/load/compile)",
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/SubprunShell',
+    packages=find_packages(exclude=[]),
+    install_requires=['martialLaw',],
+    keywords=['subpr'],
+    python_requires='>=3.4',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
